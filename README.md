8ca4a63e4b819d2a63a981855ff2e777833561f4
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-09-03 01:09:52 +0530

    completed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# API used
+https://api.nationalize.io/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [data, setData] = useState();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log(name);
+    axios.get(`https://api.nationalize.io/?name=${name}`)
+      .then(response => {
+        console.log(response.data);
+        setData(response.data.country);
+      })
+  }
   return (
     <div>
-      <h2>Hello world</h2>
+      <h2 className='title'>Predict country with name</h2>
+      <div className='container'>
+        <form onSubmit={search}>
+          <input type="search" value={name} 
+          onChange={(e) => {
+            setName(e.target.value);
+            if (e.target.value){
+              setData();
+            }
+          }
+          } 
+          placeholder="Enter a name" />
+          <button type="submit">Predict</button>
+        </form>
+        <h2>Your name is: {name}</h2>
+        <div className='results'>
+          {data?.map((country) => {
+            return(
+              <div className='country-num'>
+                <h4>{country.country_id}</h4>
+                <h5 className='percent'>{((country.probability) * 100).toFixed() } %</h5>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
